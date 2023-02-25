d29d980cd2e52d7f62a14530ed599fe3ca442d26
Willian Agostini <willian.agostini@gmail.com>
2023-02-25 20:00:57 +0000

    try reproduce error

--- index.ts
+++ index.ts
@@ -2,9 +2,15 @@ import axios, { AxiosRequestConfig, CancelToken } from "axios";
 
 function sayHello(cancelToken?: CancelToken | undefined) {
 
-    axios.interceptors.request.use(function (config) {
-        config.headers.test = 'I am only a header!';
-        return config;
-      }, null, { synchronous: true });
+  let url_ = '';
+
+  let options: AxiosRequestConfig = {
+    method: 'GET',
+    url: url_,
+    headers: {
+      Accept: "application/json"
+    },
+    cancelToken
+  } 
 
 }
